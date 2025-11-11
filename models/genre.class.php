9ae648f67a806cb9d7c1b1b849fce34da7edc2a3
Erwan Hoarau <ehoarau003@iutbayonne.univ-pau.fr>
2025-11-11 18:41:14 +0100

    ajout de la classe et du dao pour genre

---
+++ models/genre.class.php
@@ -0,0 +1,47 @@
+<?php
+
+class Genre {
+    private int $idGenre;
+    private string|null $nomgenre;
+
+    public function __construct(int $idGenre, ?string $nomgenre) {
+        $this->idGenre = $idGenre;
+        $this->nomgenre = $nomgenre;
+    }
+
+
+
+    /**
+     * Get the value of idGenre
+     */ 
+    public function getIdGenre(): int
+    {
+        return $this->idGenre;
+    }
+
+    /**
+     * Set the value of idGenre
+     *
+     */ 
+    public function setIdGenre($idGenre): void
+    {
+        $this->idGenre = $idGenre;
+    }
+
+    /**
+     * Get the value of nomgenre
+     */ 
+    public function getNomgenre(): ?string
+    {
+        return $this->nomgenre;
+    }
+
+    /**
+     * Set the value of nomgenre
+     *
+     */ 
+    public function setNomgenre(?string $nomgenre): void
+    {
+        $this->nomgenre = $nomgenre;
+    }
+}
---
+++ models/genre.dao.php
@@ -0,0 +1,40 @@
+<?php 
+
+class GenreDAO {
+    private ?PDO $pdo;
+
+    public function __construct(?PDO $pdo) {
+        $this->pdo = $pdo;
+    }
+
+    public function find(?int $idGenre): ?Genre {
+        $sql = "SELECT * FROM genre WHERE idGenre = :idGenre";
+        $stmt = $this->pdo->prepare($sql);
+        $stmt->bindParam(':idGenre', $idGenre, PDO::PARAM_INT);
+        $stmt->execute();
+        $row = $stmt->fetch(PDO::FETCH_ASSOC);
+        if ($row) {
+            return new Genre(
+                $row['idGenre'],
+                $row['nomgenre']
+            );
+        }
+        return null; 
+    }
+
+    /**
+     * Get the value of pdo
+     */
+    public function getPdo(): ?PDO
+    {
+        return $this->pdo;
+    }
+    /**
+     * Set the value of pdo
+     *
+     */
+    public function setPdo(?PDO $pdo): void
+    {
+        $this->pdo = $pdo;
+    }
+}
