56f8c6007f94b8e49f6d23e65b4d720293eb6b64
Erwan Hoarau <ehoarau003@iutbayonne.univ-pau.fr>
2025-11-11 18:42:29 +0100

    ajout des includes pour les modèles

--- include.php
+++ include.php
@@ -11,3 +11,17 @@ include_once 'config/twig.php';
 
 //Ajout du modele qui gere la connexion mysql
 include_once 'models/bd.class.php';
+
+//Ajout des modeles
+include_once 'models/chanson.class.php';
+include_once 'models/chanson.dao.php';
+include_once 'models/album.class.php';
+include_once 'models/album.dao.php';
+include_once 'models/genre.class.php';
+include_once 'models/genre.dao.php';
+include_once 'models/playlist.class.php';
+include_once 'models/playlist.dao.php';
+include_once 'models/battle.class.php';
+include_once 'models/battle.dao.php';
+include_once 'models/fichier.class.php';
+include_once 'models/fichier.dao.php';
