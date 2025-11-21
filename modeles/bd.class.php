98dd970f5d375dc6e06cc91c44279d1161954987
Erwan Hoarau <ehoarau003@iutbayonne.univ-pau.fr>
2025-11-21 15:31:38 +0100

    création du config.json et migration pour le fonctionnement pour le json

--- modeles/bd.class.php
+++ modeles/bd.class.php
@@ -8,7 +8,7 @@ class bd
     private function __construct()
     {
         try {
-            $this->pdo = new PDO('mysql:host=' . DB_HOST . ';dbname=' . DB_NAME, DB_USER, DB_PASS);
+            $this->pdo = new PDO('mysql:host=' . Constantes::getInstance()->getConfig()['db']['host'] . ';dbname=' . Constantes::getInstance()->getConfig()['db']['dbname'], Constantes::getInstance()->getConfig()['db']['username'], Constantes::getInstance()->getConfig()['db']['password']);
             $this->pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         } catch (PDOException $e) {
             die('Connexion a la BD échouée: ' . $e->getMessage());
