ac09598035bf38a10c54a30ced6601c98ad3faa6
RAMIREZ BATALLA Angel David <contact@angelbatalla.com>
2025-11-28 15:28:56 +0100

    Enum fichiers

--- modeles/role.dao.php
+++ modeles/role.dao.php
@@ -1,14 +1,5 @@
 <?php
 
-enum RoleEnum: string
-{
-    case Admin = 'admin';
-    case Artiste = 'artiste';
-    case Auditeur = 'auditeur';
-    case Producteur = 'producteur';
-    case Invite = 'invite';
-}
-
 class RoleDao
 {
 
